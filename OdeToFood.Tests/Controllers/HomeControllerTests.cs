2e34135d2b307eb45136bb9446d95011e5c8f6ba
Hendris <hendris.maar@gmail>
2022-09-29 15:37:04 +0300

    Add new project OdeToFood.Tests to test new controller action About

---
+++ OdeToFood.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OdeToFood.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void AboutTest()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+            //Act
+            ViewResult result = controller.About() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result.Model);
+        }
+    }
+}
