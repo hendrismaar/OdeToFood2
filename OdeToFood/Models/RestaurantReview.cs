6af522aae9c89503289033a5384fd41d6ac80a47
Hendris <hendris.maar@gmail>
2022-10-19 15:12:51 +0300

    Implement IValidatableObject validation

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -6,28 +6,7 @@ using System.Threading.Tasks;
 
 namespace OdeToFood.Models
 {
-	public class MaxWordsAttribute:ValidationAttribute
-    {
-		private readonly int _maxWords;
-		public MaxWordsAttribute(int maxWords):base("{0} has too many words.")
-        {
-			_maxWords = maxWords;
-        }
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            if (value != null)
-            {
-				var valueAsString = value.ToString();
-                if (valueAsString.Split(' ').Length > _maxWords)
-                {
-					var errorMessage = FormatErrorMessage(validationContext.DisplayName);
-					return new ValidationResult(errorMessage);
-                }
-            }
-			return ValidationResult.Success;
-		}
-	}
-	public class RestaurantReview
+	public class RestaurantReview:IValidatableObject
 	{
 		public int Id { get; set; }
 		[Range(1, 10)]
@@ -38,8 +17,15 @@ namespace OdeToFood.Models
 		[Display(Name = "Username")]
 		[DisplayFormat(NullDisplayText = "Anonymous")]
 		[StringLength(1024)]
-		[MaxWords(2)]
 		public string ReviewerName { get; set; }
 		public int RestaurantId { get; set; }
-	}
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Rating < 2 && ReviewerName.ToLower().StartsWith("scott"))
+            {
+				yield return new ValidationResult("Sorry, Scott, you can't do that.");
+            }
+        }
+    }
 }
