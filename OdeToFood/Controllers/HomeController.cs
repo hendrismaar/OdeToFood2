57133ed2398b4ea76178b3a851b815aeda4526b8
Hendris <hendris.maar@gmail>
2022-10-21 11:36:17 +0300

    Implement pagination to Home Index

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -7,7 +7,9 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace OdeToFood.Controllers
 {
@@ -19,20 +21,32 @@ namespace OdeToFood.Controllers
             _db = dbContext;
         }
 
-        public IActionResult Index(string searchTerm = null)
+        public ActionResult Autocomplete (string term)
+        {
+            var model =
+                _db.Restaurants
+                            .Where(r => r.Name.StartsWith(term))
+                            .Take(10)
+                            .Select(r => new
+                            {
+                                label = r.Name
+                            });
+            return Json(model);
+        }
+
+        public IActionResult Index(string searchTerm = null, int page = 1)
         {
             var model = _db.Restaurants
-                            .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
-                            .Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
-                            .Take(10)
-                            .Select(r => new RestaurantListViewModel
-                            {
-                                Id = r.Id,
-                                Name = r.Name,
-                                City = r.City,
-                                Country = r.Country,
-                                CountOfReviews = r.Reviews.Count()
-                            });
+                .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
+                .Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
+                .Select(r => new RestaurantListViewModel
+                {
+                    Id = r.Id,
+                    Name = r.Name,
+                    City = r.City,
+                    Country = r.Country,
+                    CountOfReviews = r.Reviews.Count()
+                }).ToPagedList(page, 10);
 
             if (Request.IsAjaxRequest())
             {
